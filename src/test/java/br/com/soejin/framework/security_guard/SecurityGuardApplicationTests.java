91548d1dedb253040eab4bb30615cafe53780992
Douglas Dreer <douglasdreer@outlook.com.br>
2025-04-29 03:21:27 -0300

    test: teste unitário inicial

---
+++ src/test/java/br/com/soejin/framework/security_guard/SecurityGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soejin.framework.security_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
