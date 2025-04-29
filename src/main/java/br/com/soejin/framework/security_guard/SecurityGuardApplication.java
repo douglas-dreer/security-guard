775791375069096f5d042a8c2eacb355ccdca6f9
Douglas Dreer <douglasdreer@outlook.com.br>
2025-04-29 03:20:21 -0300

    config: arquivo principal

---
+++ src/main/java/br/com/soejin/framework/security_guard/SecurityGuardApplication.java
@@ -0,0 +1,13 @@
+package br.com.soejin.framework.security_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGuardApplication.class, args);
+    }
+
+}
