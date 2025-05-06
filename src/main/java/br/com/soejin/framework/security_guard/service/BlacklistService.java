ead0b3e88d0776e5229b007d6a83e296ab1917e4
Douglas Dreer <douglasdreer@outlook.com.br>
2025-05-05 22:34:15 -0300

    feat: remove unused import in BlacklistService interface

--- src/main/java/br/com/soejin/framework/security_guard/service/BlacklistService.java
+++ src/main/java/br/com/soejin/framework/security_guard/service/BlacklistService.java
@@ -1,7 +1,5 @@
 package br.com.soejin.framework.security_guard.service;
 
-import br.com.soejin.framework.security_guard.model.Blacklist;
-
 public interface BlacklistService {
     boolean isBlacklisted(String token);
     void addTokenToBlacklist(final String token, final Long userId);
