c8e01e497d905f8366bb693847da64cf1b0c88dc
Douglas Dreer <dmd@bdmg.mg.gov.br>
2025-05-07 13:59:42 -0300

    refactor: remover dependência do JwtUtil na BlacklistServiceImpl
    Esta atualização simplifica a classe BlacklistServiceImpl ao remover a dependência do JwtUtil, que não estava sendo utilizada, melhorando a clareza e a manutenção do código.

--- src/main/java/br/com/soejin/framework/security_guard/service/impl/BlacklistServiceImpl.java
+++ src/main/java/br/com/soejin/framework/security_guard/service/impl/BlacklistServiceImpl.java
@@ -4,17 +4,14 @@ import br.com.soejin.framework.security_guard.model.Blacklist;
 import br.com.soejin.framework.security_guard.model.User;
 import br.com.soejin.framework.security_guard.repository.BlacklistRepository;
 import br.com.soejin.framework.security_guard.service.BlacklistService;
-import br.com.soejin.framework.security_guard.util.JwtUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BlacklistServiceImpl implements BlacklistService {
     private final BlacklistRepository repository;
-    private final JwtUtil jwtUtil;
-
-    public BlacklistServiceImpl(BlacklistRepository repository, JwtUtil jwtUtil) {
+    
+    public BlacklistServiceImpl(BlacklistRepository repository) {
         this.repository = repository;
-        this.jwtUtil = jwtUtil;
     }
 
     @Override
